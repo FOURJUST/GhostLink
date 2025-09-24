681150ee5841019c9c1a8676eed6b3e4c6667567
unknown <Student@SIOP0202-EDU-06.EPTM.lan>
2025-09-24 14:03:51 +0200

    Changement de nom pour le script des interraction et du script.bat

--- bat/script.bat
+++ bat/script.bat
@@ -1,10 +1,5 @@
-@echo off
-set "SCRIPT=%~dp0scriptInterraction.ps1"
+@ECHO OFF
+SET Directory=%~dp0
+SET ScriptPath="%Directory%scriptInteraction.ps1"
 
-if not exist "%SCRIPT%" (
-    echo Le fichier scriptInterraction.ps1 est introuvable dans le même dossier que ce fichier .bat.
-    pause
-    exit /b
-)
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File \"%SCRIPT%\"' -Verb RunAs"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%ScriptPath%""' -Verb RunAs}"
